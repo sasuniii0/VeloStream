d6153a0be4759d1544396e85cb1ed9f992ac8161
sasuniii0 <sasuniwijerathne@gmail.com>
2026-03-15 14:20:07 +0530

    ✨ feature(VeloStream): Add application.yml and 2 other files
    6 file(s) changed, +42/-0 lines
    
    Features:
    - D:/VeloStream/EurekaServer/src/main/resources/application.yml (+7/-0)
    - D:/VeloStream/UserService/src/main/resources/application.yml (+7/-0)
    - D:/VeloStream/WatchlistService/src/main/resources/application.yml (+7/-0)
    - D:/VeloStream/NotificationService/src/main/resources/application.yml (+7/-0)
    - D:/VeloStream/ReviewService/src/main/resources/application.yml (+7/-0)
    - D:/VeloStream/MovieService/src/main/resources/application.yml (+7/-0)

---
+++ WatchlistService/src/main/java/com/example/WatchlistService/WatchlistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.WatchlistService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistServiceApplication.class, args);
+	}
+
+}
---
+++ WatchlistService/src/test/java/com/example/WatchlistService/WatchlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WatchlistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
